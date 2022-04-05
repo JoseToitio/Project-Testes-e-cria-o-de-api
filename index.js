0a3d3219404b25a5ab0a2c473b8f3eecbb471892
JoseToitio <josetoitio@gmail.com>
2022-04-05 18:00:24 -0300

    jst timout

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const router = require('./router/router');
+
 require('dotenv').config();
 // não remova esse endpoint, e para o avaliador funcionar
 const app = express();
 app.use(express.json());
 app.use(bodyParser.json());
-app.use(router);
+
 app.get('/', (_request, response) => {
   response.send();
 });
-
+app.use(router);
 app.listen(process.env.PORT, () => {
 console.log(`Escutando na porta ${process.env.PORT}`); 
 });
